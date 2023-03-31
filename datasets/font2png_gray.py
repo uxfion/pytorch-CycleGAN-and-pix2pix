8c6539ffb8738bd0d4162f5f33b52803700f427b
Lecter <uxfion@gmail.com>
2023-04-01 01:23:31 +0800

    add font2png

---
+++ datasets/font2png_gray.py
@@ -0,0 +1,46 @@
+import os
+from PIL import ImageFont, Image, ImageDraw
+
+font_name = "times"
+ttf_path = f"./{font_name}.ttf"
+text_size = 400
+font = ImageFont.truetype(ttf_path, text_size)
+
+characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+if not os.path.exists(f"gray_font_{font_name}"):
+    os.makedirs(f"gray_font_{font_name}")
+
+for char in characters:
+    img = Image.new("L", (1200, 900), 255)
+    draw = ImageDraw.Draw(img)
+    white = 0
+    text_width = font.getbbox(char)[2]
+    text_height = font.getbbox(char)[3]
+    x = (1200 - text_width) // 2
+    y = (900 - text_height - 200) // 2
+    draw.text((x, y), char, font=font, fill=white)
+    name = char
+    if char.islower():
+        name += "_lower"
+    img.save(f"gray_font_{font_name}/{name}.png")
+
+# 随意组合两个大小写字母
+for char1 in characters[:52]:
+    for char2 in characters[:52]:
+        img = Image.new("L", (1200, 900), 255)
+        draw = ImageDraw.Draw(img)
+        white = 0
+        string = char1 + char2
+        text_width = font.getbbox(string)[2]
+        text_height = font.getbbox(string)[3]
+        x = (1200 - text_width) // 2
+        y = (900 - text_height - 200) // 2
+        draw.text((x, y), string, font=font, fill=white)
+        name1 = char1
+        name2 = char2
+        if char1.islower():
+            name1 += "_lower"
+        if char2.islower():
+            name2 += "_lower"
+        img.save(f"gray_font_{font_name}/{name1}{name2}.png")
---
+++ datasets/font2png_gray_test.py
@@ -0,0 +1,41 @@
+import os
+from PIL import ImageFont, Image, ImageDraw
+
+font_name = "times"
+ttf_path = f"./{font_name}.ttf"
+text_size = 400
+font = ImageFont.truetype(ttf_path, text_size)
+
+strings = [
+    "z",
+    "u",
+    "s",
+    "t",
+    "zu",
+    "zU",
+    "Zu",
+    "ZU",
+    "zust",
+    "Zust",
+    "ZUST",
+    "Chm",
+    "Lyh",
+]
+
+if not os.path.exists(f"test_gray_font_{font_name}"):
+    os.makedirs(f"test_gray_font_{font_name}")
+
+for string in strings:
+    img = Image.new("L", (1200, 900), 255)
+    draw = ImageDraw.Draw(img)
+    white = 0
+    text_width = font.getbbox(string)[2]
+    text_height = font.getbbox(string)[3]
+    x = (1200 - text_width) // 2
+    y = (900 - text_height - 200) // 2
+    draw.text((x, y), string, font=font, fill=white)
+    name = string
+    for s in string:
+        if s.islower():
+            name += "_lower"
+    img.save(f"test_gray_font_{font_name}/{name}.png")
