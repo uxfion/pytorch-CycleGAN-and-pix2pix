d111ae77a2b4490f5b0ff0196528e49af3c6b1c8
Lecter <uxfion@gmail.com>
2023-04-04 00:02:00 +0800

    lower letter with _ ending

--- datasets/font2png_gray.py
+++ datasets/font2png_gray.py
@@ -1,7 +1,8 @@
 import os
+import random
 from PIL import ImageFont, Image, ImageDraw
 
-font_name = "times"
+font_name = "pacifico"
 ttf_path = f"./{font_name}.ttf"
 text_size = 400
 font = ImageFont.truetype(ttf_path, text_size)
@@ -17,12 +18,12 @@ for char in characters:
     white = 0
     text_width = font.getbbox(char)[2]
     text_height = font.getbbox(char)[3]
+    # mov center
     x = (1200 - text_width) // 2
     y = (900 - text_height - 200) // 2
     draw.text((x, y), char, font=font, fill=white)
     name = char
-    if char.islower():
-        name += "_lower"
+    name = "".join([c + "_" if c.islower() else c for c in name])
     img.save(f"gray_font_{font_name}/{name}.png")
 
 # 随意组合两个大小写字母
@@ -34,13 +35,29 @@ for char1 in characters[:52]:
         string = char1 + char2
         text_width = font.getbbox(string)[2]
         text_height = font.getbbox(string)[3]
+        # mov center
         x = (1200 - text_width) // 2
         y = (900 - text_height - 200) // 2
         draw.text((x, y), string, font=font, fill=white)
-        name1 = char1
-        name2 = char2
-        if char1.islower():
-            name1 += "_lower"
-        if char2.islower():
-            name2 += "_lower"
-        img.save(f"gray_font_{font_name}/{name1}{name2}.png")
+        name = char1 + char2
+        name = "".join([c + "_" if c.islower() else c for c in name])
+        img.save(f"gray_font_{font_name}/{name}.png")
+
+# 随机随意组合四个大小写字母
+for i in range(2000):
+    img = Image.new("L", (1500, 900), 255)
+    draw = ImageDraw.Draw(img)
+    white = 0
+    string = ""
+    for j in range(4):
+        char = random.choice(characters[:52])
+        string += char
+    text_width = font.getbbox(string)[2]
+    text_height = font.getbbox(string)[3]
+    # mov center
+    x = (1200 - text_width) // 2
+    y = (900 - text_height - 200) // 2
+    draw.text((x, y), string, font=font, fill=white)
+    name = string[0] + string[1] + string[2] + string[3]
+    name = "".join([c + "_" if c.islower() else c for c in name])
+    img.save(f"gray_font_{font_name}/{name}.png")
--- datasets/font2png_gray_test.py
+++ datasets/font2png_gray_test.py
@@ -26,16 +26,14 @@ if not os.path.exists(f"test_gray_font_{font_name}"):
     os.makedirs(f"test_gray_font_{font_name}")
 
 for string in strings:
-    img = Image.new("L", (1200, 900), 255)
+    img = Image.new("L", (1500, 900), 255)
     draw = ImageDraw.Draw(img)
     white = 0
     text_width = font.getbbox(string)[2]
     text_height = font.getbbox(string)[3]
-    x = (1200 - text_width) // 2
+    x = (1500 - text_width) // 2
     y = (900 - text_height - 200) // 2
     draw.text((x, y), string, font=font, fill=white)
     name = string
-    for s in string:
-        if s.islower():
-            name += "_lower"
+    name = "".join([c + "_" if c.islower() else c for c in name])
     img.save(f"test_gray_font_{font_name}/{name}.png")
