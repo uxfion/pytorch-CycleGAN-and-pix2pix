d05cb832ca167113ce6e24e57828d137e631054c
Lecter <uxfion@gmail.com>
2023-04-07 23:44:43 +0800

    rewrite blur

--- datasets/blur.py
+++ datasets/blur.py
@@ -1,96 +1,65 @@
-import cv2
+from PIL import Image, ImageFilter
 import os
-import numpy as np
-import tqdm
 import random
 
+class ImageProcessor:
+    def __init__(self, path):
+        self.path = path
 
-class Blur:
-    def __init__(self, input_path, output_path, type, times):
-        self.input_path = input_path
-        self.output_path = output_path
-        self.type = type
-        self.times = times
-
-    def down_up(self, img_path):
-        img_hr = cv2.imread(img_path)
-        img = img_hr
-        for i in range(self.times):
-            img = cv2.pyrDown(img)
-            img = cv2.pyrUp(img)
-        img_lr = img
-        return img_lr
-
-    # gaussian noise
-    def gaussian(self, img_path):
-        mean = 0
-        var = 0.002
-        image = cv2.imread(img_path)
-        image = np.array(image / 255, dtype=float)  # 将原始图像的像素值进行归一化，除以255使得像素值在0-1之间
-        noise = np.random.normal(
-            mean, var**0.5, image.shape
-        )  # 创建一个均值为mean，方差为var呈高斯分布的图像矩阵
-        out = image + noise  # 将噪声和原始图像进行相加得到加噪后的图像
-        if out.min() < 0:
-            low_clip = -1.0
+    def blur_single(self, n_range, method):
+        if isinstance(n_range, int):
+            n = n_range
+        elif isinstance(n_range, tuple) and len(n_range) == 2:
+            n = random.randint(n_range[0], n_range[1])
         else:
-            low_clip = 0.0
-        out = np.clip(
-            out, low_clip, 1.0
-        )  # clip函数将元素的大小限制在了low_clip和1之间了，小于的用low_clip代替，大于1的用1代替
-        out = np.uint8(out * 255)  # 解除归一化，乘以255将加噪后的图像的像素值恢复
-        # cv.imshow("gasuss", out)
-        noise = noise * 255
-        return out
+            raise ValueError("Invalid n_range")
+        image = Image.open(self.path)
+        if method == "Gaussian":
+            blurred_image = image.filter(ImageFilter.GaussianBlur(radius=n))
+        elif method == "Box":
+            blurred_image = image.filter(ImageFilter.BoxBlur(radius=n))
+        elif method == "Median":
+            blurred_image = image.filter(ImageFilter.MedianFilter(size=n))
+        else:
+            raise ValueError("Invalid blur method")
+        file_name, ext = os.path.splitext(self.path)
+        blurred_image.save(f"{file_name}_{method}_{n}{ext}")
 
-    # salt and pepper noise
-    def salt_pepper(self, img_path):
-        prob = 0.01
-        image = cv2.imread(img_path)
-        output = np.zeros(image.shape, np.uint8)
-        noise_out = np.zeros(image.shape, np.uint8)
-        thres = 1 - prob
-        for i in range(image.shape[0]):
-            for j in range(image.shape[1]):
-                rdn = random.random()  # 随机生成0-1之间的数字
-                if rdn < prob:  # 如果生成的随机数小于噪声比例则将该像素点添加黑点，即椒噪声
-                    output[i][j] = 0
-                    noise_out[i][j] = 0
-                elif rdn > thres:  # 如果生成的随机数大于（1-噪声比例）则将该像素点添加白点，即盐噪声
-                    output[i][j] = 255
-                    noise_out[i][j] = 255
+    def blur_folder(self, n_range, method):
+        folder_name = os.path.basename(self.path)
+        new_folder_name = f"{folder_name}_{method}"
+        os.makedirs(new_folder_name, exist_ok=True)
+        for file_name in os.listdir(self.path):
+            file_path = os.path.join(self.path, file_name)
+            if os.path.isfile(file_path):
+                if isinstance(n_range, int):
+                    n = n_range
+                elif isinstance(n_range, tuple) and len(n_range) == 2:
+                    n = random.randint(n_range[0], n_range[1])
                 else:
-                    output[i][j] = image[i][j]  # 其他情况像素点不变
-                    noise_out[i][j] = 100
-        # result = [noise_out,output]#返回椒盐噪声和加噪图像
-        result = output  # 返回椒盐噪声和加噪图像
-        return result
+                    raise ValueError("Invalid n_range")
+                image = Image.open(file_path)
+                if method == "Gaussian":
+                    blurred_image = image.filter(ImageFilter.GaussianBlur(radius=n))
+                elif method == "Box":
+                    blurred_image = image.filter(ImageFilter.BoxBlur(radius=n))
+                elif method == "Median":
+                    blurred_image = image.filter(ImageFilter.MedianFilter(size=n))
+                else:
+                    raise ValueError("Invalid blur method")
+                new_file_name = f"{os.path.splitext(file_name)[0]}_{method}_{n}{os.path.splitext(file_name)[1]}"
+                new_file_path = os.path.join(new_folder_name, new_file_name)
+                blurred_image.save(new_file_path)
 
-    def blur_all(self):
-        for root, dirs, files in os.walk(input_path):
-            if not os.path.exists(root.replace(self.input_path, self.output_path)):
-                os.makedirs(root.replace(self.input_path, self.output_path))
-            for file in tqdm.tqdm(files):
-                img_path = os.path.join(root, file)
-                if self.type == "down_up":
-                    img = self.down_up(img_path)
-                    file = file.split(".")[0] + "_down_up.jpg"
-                elif self.type == "gaussian":
-                    img = self.gaussian(img_path)
-                    file = file.split(".")[0] + "_gaussian.jpg"
-                elif self.type == "salt_pepper":
-                    img = self.salt_pepper(img_path)
-                    file = file.split(".")[0] + "_salt_pepper.jpg"
-                output_img_path = os.path.join(
-                    root.replace(self.input_path, self.output_path), file
-                )
-                cv2.imwrite(output_img_path, img)
+    def blur(self, n_range, method):
+        if os.path.isfile(self.path):
+            self.blur_single(n_range, method)
+        elif os.path.isdir(self.path):
+            self.blur_folder(n_range, method)
+        else:
+            raise ValueError("Invalid path")
+        
 
 
-if __name__ == "__main__":
-    # input_path = "./lr2hr/testB"
-    input_path = "./ultrasound_test"
-    output_path = "./ultrasound_test_blur"
-
-    blur = Blur(input_path, output_path, "down_up", 20)
-    blur.blur_all()
+image_processor = ImageProcessor("./coco/trainB/")
+image_processor.blur((0,10), "Gaussian")
