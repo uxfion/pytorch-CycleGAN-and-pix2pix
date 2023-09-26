5c44496bc0d514b2dd8e6dcaeb48bd0729431680
Lecter <uxfion@gmail.com>
2023-09-26 17:56:53 +0800

    输入图像的A与B对比度一致

--- data/my_aligned_dataset.py
+++ data/my_aligned_dataset.py
@@ -3,6 +3,7 @@ from data.base_dataset import BaseDataset, get_params, get_transform
 from data.image_folder import make_dataset
 from PIL import Image, ImageFilter
 import random
+import numpy as np
 
 
 class MyAlignedDataset(BaseDataset):
@@ -61,6 +62,7 @@ class MyAlignedDataset(BaseDataset):
         A = AB.filter(ImageFilter.GaussianBlur(radius=n_A))
         B = AB.filter(ImageFilter.GaussianBlur(radius=n_B))
 
+        A = self.mapped(B, A)
 
         # apply the same transform to both A and B
         transform_params = get_params(self.opt, A.size)
@@ -86,3 +88,39 @@ class MyAlignedDataset(BaseDataset):
     def __len__(self):
         """Return the total number of images in the dataset."""
         return len(self.AB_paths)
+
+    def mapped(self, img1, img2):
+        # 将 img1 和 img2 分解为三个通道
+        img1_r, img1_g, img1_b = img1.split()
+        img2_r, img2_g, img2_b = img2.split()
+
+        # 分别对这三个通道进行处理
+        img2_r = self.mapped_single_channel(img1_r, img2_r)
+        img2_g = self.mapped_single_channel(img1_g, img2_g)
+        img2_b = self.mapped_single_channel(img1_b, img2_b)
+
+        # 将处理后的三个通道合并为一张彩色图像
+        img2 = Image.merge("RGB", (img2_r, img2_g, img2_b))
+
+        return img2
+
+    def mapped_single_channel(self, img1, img2):
+        img1_pixels = np.sort(np.array(img1).flatten())
+        img2_pixels = np.sort(np.array(img2).flatten())
+
+        img1_low = float(img1_pixels[int(len(img1_pixels) * 0.05)])
+        img1_high = float(img1_pixels[int(len(img1_pixels) * 0.95)])
+        img2_low = float(img2_pixels[int(len(img2_pixels) * 0.05)])
+        img2_high = float(img2_pixels[int(len(img2_pixels) * 0.95)])
+
+        img2_array = np.array(img2, dtype=float)
+
+        # scale_factor = ((img2_array - img2_low) / (img2_high - img2_low)) * (img1_high - img1_low) + img1_low
+        # scale_factor = np.clip(scale_factor, 0, 255, out=scale_factor)
+
+        # # Adding a small epsilon value to the denominator
+        epsilon = 1e-5
+        scale_factor = ((img2_array - img2_low) / (img2_high - img2_low + epsilon)) * (img1_high - img1_low) + img1_low
+        scale_factor = np.clip(scale_factor, 0, 255, out=scale_factor)
+
+        return Image.fromarray(scale_factor.astype(np.uint8))
