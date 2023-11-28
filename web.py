3e6e3e321ddbcc4fa84ed9ed61287bd7e9f50fb6
Lecter <uxfion@gmail.com>
2023-11-28 14:55:53 +0800

    split complete

--- web.py
+++ web.py
@@ -1,138 +1,39 @@
 import streamlit as st
-from PIL import Image, ImageFilter
 import io
-import torch
-from torchvision import transforms
-from models import create_model
-from options.test_options import TestOptions
-import numpy as np
-
 import datetime
+from PIL import Image, ImageFilter
+
+from cyclegan_infer import load_cyclegan_model, cyclegan_infer
 
 # Set page config to make the layout use the full page width
 st.set_page_config(layout="wide")
 
-model = None
+
+cyclegan_model = load_cyclegan_model()
 
 
-@st.cache_resource
-def load_model():
-    global model
+# def crop_to_divisible_by_four(img):
+#     # Check if dimensions are already divisible by 4
+#     if img.width % 4 == 0 and img.height % 4 == 0:
+#         return img
 
-    opt = TestOptions().parse()
-    opt.num_threads = 0
-    opt.batch_size = 1
-    opt.serial_batches = True
-    opt.no_flip = True
-    opt.display_id = -1
+#     print(f"图像尺寸{img.size}不能被4整除, 裁剪图像至能被4整除...")
+#     # Calculate the new dimensions
+#     new_width = img.width - (img.width % 4)
+#     new_height = img.height - (img.height % 4)
+#     print(f"裁剪后图像尺寸: {new_width} x {new_height}")
+#     # Crop the image
+#     cropped_img = img.crop((0, 0, new_width, new_height))
 
-    # opt.dataroot="./datasets/xijing/low_quality"
-    # opt.name="ultrasound_2023_10_10_batch5"
-    # opt.model="my_test"
-    # opt.no_dropout=True
-    # opt.preprocess="none"
-    # opt.gpu_ids=[1]
-
-    # Load the model
-    model = create_model(opt)
-    model.setup(opt)
-    model.eval()
-    return model
+#     return cropped_img
 
 
-model = load_model()
-# if not model:
-#     print("Loading model...")
-#     model = load_model()
-
-
-def crop_to_divisible_by_four(img):
-    # Check if dimensions are already divisible by 4
-    if img.width % 4 == 0 and img.height % 4 == 0:
-        return img
-
-    print(f"图像尺寸{img.size}不能被4整除, 裁剪图像至能被4整除...")
-    # Calculate the new dimensions
-    new_width = img.width - (img.width % 4)
-    new_height = img.height - (img.height % 4)
-    print(f"裁剪后图像尺寸: {new_width} x {new_height}")
-    # Crop the image
-    cropped_img = img.crop((0, 0, new_width, new_height))
-
-    return cropped_img
-
-
-# 调整第二张图像img2的亮度和对比度，使其与第一张图像img1相似。
-def mapped(img1, img2):
-    # 将 img1 和 img2 分解为三个通道
-    img1_r, img1_g, img1_b = img1.split()
-    img2_r, img2_g, img2_b = img2.split()
-
-    # 分别对这三个通道进行处理
-    img2_r = mapped_single_channel(img1_r, img2_r)
-    img2_g = mapped_single_channel(img1_g, img2_g)
-    img2_b = mapped_single_channel(img1_b, img2_b)
-
-    # 将处理后的三个通道合并为一张彩色图像
-    img2 = Image.merge("RGB", (img2_r, img2_g, img2_b))
-
-    return img2
-
-
-def mapped_single_channel(img1, img2):
-    img1_pixels = np.sort(np.array(img1).flatten())
-    img2_pixels = np.sort(np.array(img2).flatten())
-
-    img1_low = float(img1_pixels[int(len(img1_pixels) * 0.05)])
-    img1_high = float(img1_pixels[int(len(img1_pixels) * 0.95)])
-    img2_low = float(img2_pixels[int(len(img2_pixels) * 0.05)])
-    img2_high = float(img2_pixels[int(len(img2_pixels) * 0.95)])
-
-    img2_array = np.array(img2, dtype=float)
-    scale_factor = ((img2_array - img2_low) / (img2_high - img2_low)) * (img1_high - img1_low) + img1_low
-    scale_factor = np.clip(scale_factor, 0, 255, out=scale_factor)
-
-    return Image.fromarray(scale_factor.astype(np.uint8))
-
-
-# Function to perform inference
-def infer(model, image_raw, sigma):
-    # image_raw = crop_to_divisible_by_four(image_raw)
-    # Apply necessary transformations
-    transform = transforms.Compose([
-        # transforms.Lambda(lambda img: __make_power_2(img, base=4)),
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
-    image = transform(image_raw).unsqueeze(0)
-
-    # Perform inference
-    with torch.no_grad():
-        target_code = torch.zeros((1, 20))
-        target_code[0, sigma] = 1
-        target_code = target_code.to("cuda:1")
-        fake_image = model.netG(image.to("cuda:1"), target_code)
-
-    # Convert to PIL image and return
-    fake_image = (fake_image.cpu().squeeze(0) + 1) / 2  # Denormalize
-    fake_image = transforms.ToPILImage()(fake_image)
-
-    contrast_image = mapped(image_raw, fake_image)
-
-    return contrast_image
-
-
-# Dummy function for image quality improvement (replace with your actual model inference)
-def improve_image_quality(image, model, parameter):
-    # Your image processing code here
-    # For now, it just returns the original image
+def test_infer(model, image, parameter):
     improved_image = image.filter(ImageFilter.GaussianBlur(radius=parameter))
-
     return improved_image
 
 
 # ========== web ==========================
-
 # Title of the webpage
 st.title('Image Quality Improvement of Mobile Ultrasound Devices')
 
@@ -144,7 +45,7 @@ with col1:
 
 # Dropdown for model selection
 with col2:
-    model_select = st.selectbox('Model select', ['Super-Resolution Model', 'Model 2', 'Model 3'])
+    model_select = st.selectbox('Model select', ['Super-Resolution Model', 'Model 2', 'Model 3', 'Test'])
 
 parameter_col, button_col = st.columns(2)
 with parameter_col:
@@ -170,8 +71,21 @@ if infer_button:
         print(f"  - 尺寸: {image.size}")
         print(f"  - 格式: {image.format}")
         print(f"  - exif: {image.getexif()}")
-        improved_image = infer(model, image, parameter)
-        print(f"\n{datetime.datetime.now()} 推理完成\n\n\n")
+
+        print(f"推理模型: {model_select}")
+
+        if model_select == 'Super-Resolution Model':
+            improved_image = cyclegan_infer(cyclegan_model, image, parameter)
+        elif model_select == 'Model 2':
+            improved_image = image
+        elif model_select == 'Model 3':
+            improved_image = image
+        elif model_select == 'Test':
+            improved_image = test_infer(None, image, parameter)
+        else:
+            improved_image = image
+
+        print(f"{datetime.datetime.now()} 推理完成\n\n\n")
 
         # Display the image after improvement
         with col2:
